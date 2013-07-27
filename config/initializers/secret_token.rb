5b42d80749b4d222e011abe2c072fcb49af86bef
emmath <emmatrevena@gmail.com>
2013-07-27 12:37:32 -0700

    Generate the Life Cycle application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeCycle::Application.config.secret_token = '4ce5ed9ed75b5ac48c94abc9e1b0b1d46b44d7d2f0b11730128d5dc331e6344d6d57a025741101b2cf1f1e1af312bab0949a725a993849c9586fc91bef267837'
